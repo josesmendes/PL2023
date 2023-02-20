c593a7b2be38cee17d12c010af8818888afc83f0
me <nao.ha.este@gmail.com>
2023-02-20 15:28:32 +0000

    update

--- TPC1/main.py
+++ TPC1/main.py
@@ -1,109 +1,50 @@
 from os.path import exists
 
 
-def por_doenca(path):
-    fst = True
-    doente = [0, 0]
-    saudvl = [0, 0]
-    file = open(path, 'r')
-    for line in file.readlines():
-        if fst:
-            fst = False
-        else:
-            parsed = line.split(',')
-            if int(parsed[5]) == 1:
-                if parsed[1] == 'M':
-                    doente[0] += 1
-                else:
-                    doente[1] += 1
-            else:
-                if parsed[1] == 'M':
-                    saudvl[0] += 1
-                else:
-                    saudvl[1] += 1
-    file.close()
-    print(doente)
-    print(saudvl)
+def por_doenca(lines):
+    homem = [x for x in lines if x[1] == 'M' and x[-1]]
+    mulher = [x for x in lines if x[1] == 'F' and x[-1]]
+    print(f'Homes-->{len(homem)/len(lines)}\nMulheres-->{len(mulher)/len(lines)}')
 
 
-def por_idade(path):
-    fst = True
-    doente = {}
-    saudvl = {}
-    file = open(path, 'r')
-    for line in file.readlines():
-        if fst:
-            fst = False
-        else:
-            parsed = line.split(',')
-            if int(parsed[5]) == 1:
-                escalao = int(parsed[0]) // 5
-                val = doente.get(escalao)
-                if val is None:
-                    doente[escalao] = 1
-                else:
-                    doente[escalao] = val + 1
-            # else:
-            #  val = doente.get(x)
-            #         if val is None:
-            #             val = 0  escalao = int(parsed[0]) // 5
-            #    val = saudvl.get(int(parsed[0]) // 5)
-            #    if val is None:
-            #        saudvl[escalao] = 1
-            #    else:
-            #        saudvl[escalao] = val + 1
-    file.close()
+def por_idade(lines):
+    idade_maxima = max(int(x[0]) for x in lines)
+    distrib_idades = {}
+    for idade in range(30, idade_maxima, 5):
+        pessoas = [x for x in lines if int(x[0]) in range(idade, idade + 5)]
+        doentes = [x for x in pessoas if int(x[-1])]
+        distrib_idades[(idade, idade + 4)] = len(doentes) / len(pessoas)
     print('-----------------porIdade------------------')
-    for x in range(0, max(doente.keys()) + 1):
-        val = doente.get(x)
-        if val is None:
-            val = 0
-        print(f"[{x * 5}-{((x + 1) * 5) - 1}]-->{val}")
-    # print(doente)
-    # print(saudvl)
+    for x in distrib_idades.keys():
+        print(f'{x}-->{distrib_idades.get(x)}')
 
 
-def por_col(path):
-    fst = True
-    doente = {}
-    saudvl = {}
-    file = open(path, 'r')
-    for line in file.readlines():
-        if fst:
-            fst = False
-        else:
-            parsed = line.split(',')
-            if int(parsed[5]) == 1:
-                escalao = int(parsed[3]) // 10
-                val = doente.get(escalao)
-                if val is None:
-                    doente[escalao] = 1
-                else:
-                    doente[escalao] = val + 1
-            # else:
-            #   escalao = int(parsed[3])//10
-            #  val = saudvl.get(escalao)
-            # if val is None:
-            #    saudvl[escalao] = 1
-            # else:
-            #   saudvl[escalao] = val + 1
-    file.close()
+def por_col(lines):
+    colesterol_max = max(int(x[3]) for x in lines)
+    colesterol_min = min(int(x[3]) for x in lines if int(x[3]) != 0)
+    distrib_col = {}
     print('-----------------Colesterol------------------')
-    for x in range(0, max(doente.keys()) + 1):
-        val = doente.get(x)
-        if val is None:
-            val = 0
-        print(f"[{x * 10}-{((x + 1) * 10) - 1}]-->{val}")
-    # print(doente)
-    # print(saudvl)
+    for colesterol in range(colesterol_min, colesterol_max, 10):
+        pessoas = [x for x in lines if int(x[3]) in range(colesterol, colesterol + 10)]
+        doentes = [x for x in pessoas if int(x[-1])]
+        if pessoas and doentes:
+            distrib_col[(colesterol, colesterol + 9)] = len(doentes) / len(pessoas)
+        else:
+            distrib_col[(colesterol, colesterol + 9)] = 0
+    for x in distrib_col.keys():
+        print(f'{x}-->{distrib_col.get(x)}')
 
 
 def main():
     path = '/home/me/PL2023/TPC1/myheart.csv'
     if exists(path):
-        por_doenca(path)
-        por_idade(path)
-        por_col(path)
+        valid_lines = []
+        with open(path) as file:
+            for line in file.readlines()[1::]:
+                valid_lines.append(tuple(line.strip().split(',')))
+            por_doenca(valid_lines)
+            por_idade(valid_lines)
+            por_col(valid_lines)
 
 
 if __name__ == "__main__":
